ab9c084f36237f91a7353b5b2da78dbb8b5e246e
Aleksandar Pajevic <aleksandar.pajevic@gmail.com>
2020-02-20 15:01:54 +0100

    joining main namespace, sending back NS info, update DOM with NS

--- SocketIO_201/slack/public/scripts.js
+++ SocketIO_201/slack/public/scripts.js
@@ -1,13 +1,26 @@
 const socket = io('http://localhost:9000') //the / namespace-endpoint
-const socket2 = io('http://localhost:9000/admin') // the /admin namespace
+
+socket.on('nsList', (nsData)=>{
+  console.log('the list of namespaces has arrived!!')
+  let namespacesDiv = document.querySelector('.namespaces');
+  namespacesDiv.innerHTML = " ";
+  nsData.forEach( (ns) => {
+    namespacesDiv.innerHTML += `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.img}" /></div>`
+  });
+  // add a click listener for each namespace
+  Array.from(document.getElementsByClassName('namespace')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+      const nsEndpoint = element.getAttribute('ns');
+      console.log(`I should now go to ${nsEndpoint}`)
+    })
+  })
+})
+
 
 socket.on('messageFromServer', (dataFromServer) => {
   console.log(dataFromServer);
 });
 
-socket2.on('welcome', (data)=>{
-  console.log(data);
-})
 
 document.querySelector('#message-form').addEventListener('submit', (event) => {
   event.preventDefault();
--- SocketIO_201/slack/slack.js
+++ SocketIO_201/slack/slack.js
@@ -2,23 +2,33 @@ const socketio = require('socket.io');
 const express = require('express');
 const app = express();
 
+let namespaces = require('./data/namespaces');
+console.log(namespaces);
+
 app.use(express.static(__dirname + '/public'));
 
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
 
+//io.on = io.of('/').on
 io.on('connection', (socket)=>{
-    socket.emit('messageFromServer', {data:'Hello from server!'});
-    socket.on('dataToServer', (dataFromClient)=>{
-        console.log(dataFromClient);
+    //build an array to send back with the img and endpoint for each NS
+    let nsData = namespaces.map((ns)=>{
+        return {
+            img: ns.img,
+            endpoint: ns.endpoint,
+        }
     })
-    socket.on('userMessageToServer', (msg)=>{
-        // console.log(msg)
-        io.of('/').emit('messageToClients', {text: msg.text})
-    })
+    // console.log('nsData:', nsData)
+    // send the nsData back to the client. We need to use socket, NOT io, because we want it to go to just this client.
+    socket.emit('nsList', nsData);
 })
 
-io.of('/admin').on('connection', (socket)=>{
-    console.log('someone connected to Admin namespace');
-    io.of('/admin').emit('welcome', 'Admin, hello from Server!')
+
+
+// loop through each namespace and listen for a connection
+namespaces.forEach((namespace)=>{
+    io.of(namespace.endpoint).on('connection', (socket)=>{
+        console.log(`${socket.id} has join ${namespace.endpoint}`)
+    })
 })
